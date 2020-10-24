c0a343189c410d6d4ac2ae4404789f75cf5e9528
Carly Pecora <carly@carlypecora.com>
2020-10-24 18:15:56 -0400

    netlify deploy

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
 			<div className="about-txt">
 				<h2 className="about-title">Hi there!</h2>
 				<p className="about-p">
-					I'm Carly! I lived in New York City, working both in the restaurant industry and in performing arts, until attending the Flatiron School in Downtown New York. I am an energetic, motivated Software Engineer currently working as a Fullstack Developer in Boston, Massachusetts.
+					I'm Carly! I am an energetic, motivated Software Engineer currently working as a Fullstack Developer in Boston, Massachusetts. Previously I lived in New York City, working both in the restaurant industry and in performing arts, until attending the Flatiron School in Downtown New York.
 				</p>
 			</div>
 		</div>
